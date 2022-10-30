9aa9abcca7b2d08cc43e59504238d67fb4dec942
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-10-30 16:30:47 +0600

    logo set up for responsive

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import home_background from "../../images/home-bg.png";
+
 
 const Home = () => {
     return (
         <div className='w-full h-screen relative bg-img'>
-            {/* <img className='bg-img' src={home_background} alt="" /> */}
+            
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,12 +12,11 @@ const Navbar = () => {
 
     return (
         <div className='flex justify-between items-center h-24 w-full px-4 absolute z-10'>
-            {/* <h1 className='w-full text-3xl font-bold text-[#F9A51A]'>Travel.</h1> */}
             <img className='logo' src={LOGO} alt="" />
 
-            <div className='flex justify-between items-center border border-gray-600 p-2 rounded input-background'>
-                <BiSearch className='pr-2' size={25} />
-                <input className='input' style={{outline:"none"}} type="text" name="" id="" placeholder='Seasch your Destination...' />
+            <div className='flex justify-between items-center border border-white p-2 rounded input-background'>
+                <BiSearch className='pr-2' style={{color:'white'}} size={25} />
+                <input className='input' style={{outline:"none", background:"none"}} type="text" name="" id="" placeholder='Seasch your Destination...' />
             </div>
 
             <ul className='hidden md:flex'>
@@ -27,11 +26,13 @@ const Navbar = () => {
                 <li className='p-4 text-white'>Contact</li>
                 <li className='mt-4 mb-4 pl-2 pr-2 bg-[#F9A51A] rounded'>Login</li>
             </ul>
+
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
-            <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-                <h1 className='w-full text-3xl font-bold text-[#F9A51A] m-4'>Travel.</h1>
+
+            <ul className={nav ? 'fixed left-3 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+            <img className='logo' src={LOGO} alt="" />
                 <li className='p-4 border-b border-gray-600 text-white'>News</li>
                 <li className='p-4 border-b border-gray-600 text-white'>Destination</li>
                 <li className='p-4 border-b border-gray-600 text-white'>Blog</li>
