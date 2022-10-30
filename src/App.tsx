7b0a6e259b907a3f375342583b09a4e4ca6b04fd
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-10-30 14:42:14 +0600

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import { Counter } from "./features/counter/Counter";
+import Navbar from "./components/Navbar";
+// import { Counter } from "./features/counter/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <Navbar />
+      {/* <header className="App-header">
         <Counter />
         <h2>Welcome to the SIX AVENGERS TRAVEL AGENCY</h2>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
     return (
-        <div>
-            
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+            <h1 className='w-full text-3xl font-bold text-[#F9A51A]'>Travel.</h1>
+
+            <div className='flex justify-between items-center border border-gray-600 p-2 rounded'>
+                <BiSearch className='pr-2' size={25} />
+                <input style={{outline:"none"}} type="text" name="" id="" placeholder='Seasch your Destination...' />
+            </div>
+
+            <ul className='hidden md:flex'>
+                <li className='p-4'>News</li>
+                <li className='p-4'>Destination</li>
+                <li className='p-4'>Blog</li>
+                <li className='p-4'>Contact</li>
+                <li className='mt-4 mb-4 pl-2 pr-2 bg-[#F9A51A] rounded'>Login</li>
+            </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+            <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+                <h1 className='w-full text-3xl font-bold text-[#F9A51A] m-4'>Travel.</h1>
+                <li className='p-4 border-b border-gray-600 text-white'>News</li>
+                <li className='p-4 border-b border-gray-600 text-white'>Destination</li>
+                <li className='p-4 border-b border-gray-600 text-white'>Blog</li>
+                <li className='p-4 border-b border-gray-600 text-white'>Contact</li>
+                <li className='p-4 text-white'>Login</li>
+            </ul>
         </div>
     );
 };
