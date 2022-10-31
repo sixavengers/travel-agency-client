4492b9fe50406ab39a874f0bb67d797a8328f097
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-10-31 19:47:18 +0600

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
+import Login from "./pages/login-register/Login";
 import "./utilities/CustomClass.css";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Login />
     </div>
   );
 }
