6968ea1d42d6e3b20036f84c1bf8dc0024c71a96
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-11-04 17:30:36 +0600

    responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
-import Login from "./pages/login-register/Login";
-import Register from "./pages/login-register/Register";
+// import Login from "./pages/login-register/Login";
+// import Register from "./pages/login-register/Register";
 import "./utilities/CustomClass.css";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
-      <Login />
-      <Register />
+      {/* <Login />
+      <Register /> */}
     </div>
   );
 }
