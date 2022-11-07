8f78ebf092370b24a4ced3ce2aa52f614754967f
ashikmahmud <ashikmamud187@gmail.com>
2022-11-07 16:40:26 +0600

    added routes for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import Login from "./pages/login-register/Login";
 import Register from "./pages/login-register/Register";
 
@@ -15,6 +16,9 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* dashboard routes */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
