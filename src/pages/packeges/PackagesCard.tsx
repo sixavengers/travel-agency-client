d3e2eccfde0804ca16bf0817f4002cdf22b8744d
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-11-07 21:54:00 +0600

    shadow added

--- src/pages/packeges/PackagesCard.tsx
+++ src/pages/packeges/PackagesCard.tsx
@@ -28,8 +28,7 @@ const PackagesCard = (props: Props) => {
                             </p>
                             {text.length > 150 ? <p className='max-w-[600px]  font-poppins mt-2'>{text.slice(0, 150)}...</p> : <p className='max-w-[600px]  font-poppins mt-2'>{text}</p>}
 
-
-                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full'>Available Tickets: 16</p>
+                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full shadow-md'>Available Tickets: 16</p>
 
                             <div className="flex items-center mt-1">
                                 <svg aria-hidden="true" className="w-5 h-5 text-theme-secondary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -74,7 +73,7 @@ const PackagesCard = (props: Props) => {
                             {text.length > 150 ? <p className='max-w-[600px]  font-poppins mt-2'>{text.slice(0, 150)}...</p> : <p className='max-w-[600px]  font-poppins mt-2'>{text}</p>}
 
 
-                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full'>Available Tickets: 12</p>
+                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full shadow-md'>Available Tickets: 12</p>
 
                             <div className="flex items-center mt-1">
                                 <svg aria-hidden="true" className="w-5 h-5 text-theme-secondary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -119,7 +118,7 @@ const PackagesCard = (props: Props) => {
                             {text.length > 150 ? <p className='max-w-[600px]  font-poppins mt-2'>{text.slice(0, 150)}...</p> : <p className='max-w-[600px]  font-poppins mt-2'>{text}</p>}
 
 
-                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full'>Available Tickets: 5</p>
+                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full shadow-md'>Available Tickets: 5</p>
 
                             <div className="flex items-center mt-1">
                                 <svg aria-hidden="true" className="w-5 h-5 text-theme-secondary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -164,7 +163,7 @@ const PackagesCard = (props: Props) => {
                             {text.length > 150 ? <p className='max-w-[600px]  font-poppins mt-2'>{text.slice(0, 150)}...</p> : <p className='max-w-[600px]  font-poppins mt-2'>{text}</p>}
 
 
-                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full'>Available Tickets: 46</p>
+                            <p className='font-poppins mt-2 bg-[#1e88e5]/90 inline-block text-white px-2 rounded-full shadow-md'>Available Tickets: 46</p>
 
                             <div className="flex items-center mt-1">
                                 <svg aria-hidden="true" className="w-5 h-5 text-theme-secondary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
