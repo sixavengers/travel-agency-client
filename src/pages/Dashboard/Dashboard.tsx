3c45ca8f4eff8e5d91c0ec57df36e10d59e0b53b
ashikmahmud <ashikmamud187@gmail.com>
2022-11-07 17:27:13 +0600

    done almost design to the dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -44,23 +44,38 @@ const Dashboard = (props: Props) => {
 
       {/* dashboard main content */}
       <div className="dashboard__main">
-        <div className="dashboard__main__header">
-          <div className="dashboard__main__header__left">
+        <div className="dashboard__main__header bg-slate-50 shadow-sm rounded-lg p-3">
+          <div className="dashboard__main__header__left flex items-center justify-between">
             <div className="dashboard__main__header__left__search">
-              <input type="text" placeholder="Search" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="p-3 rounded-md outline-none border bg-[#F8F9FC] border-gray-300 sm:w-96"
+              />
             </div>
 
-            <div className="dashboard__main__header__left__user">
-              <img src={""} alt="user" />
-              <span className="dashboard__main__header__left__user__name">
-                John Doe
-              </span>
+            <div className="dashboard__main__header__left__user flex items-center gap-2 bg-white rounded-lg border px-2 py-1 cursor-pointer">
+              <img
+                src={
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq2-DHbjWwdCcYymoNzbsnaz8KeoXzVUaShnQF3-Qn&s"
+                }
+                alt="user"
+                className="w-10 h-10 rounded-full object-cover border-2 border-gray-100"
+              />
+              <div className="flex flex-col items-start">
+                <span className="dashboard__main__header__left__user__name font-medium text-sm">
+                  John Doe
+                </span>
+                <span className="dashboard__main__header__left__user__role text-xs text-slate-400">
+                  Admin
+                </span>
+              </div>
             </div>
           </div>
         </div>
         {/* content goes here */}
-        <div className="dashboard__main__content">
-          <div className="dashboard__main__content__header">
+        <div className="dashboard__main__content py-10">
+          <div className="dashboard__main__content__header ">
             <h1 className="dashboard__main__content__header__title">
               Dashboard
             </h1>
