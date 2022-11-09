a8456f9b722b3cb3e641bfcef7754534cd4ebe25
ashikmahmud <ashikmamud187@gmail.com>
2022-11-09 23:52:41 +0600

    added input for add packages

--- src/pages/Dashboard/Packages/AddPackage/AddPackage.tsx
+++ src/pages/Dashboard/Packages/AddPackage/AddPackage.tsx
@@ -1,7 +1,135 @@
+import { Link } from "react-router-dom";
+
 type Props = {};
 
 const AddPackage = (props: Props) => {
-  return <div>AddPackage</div>;
+  return (
+    <div>
+      <div className="p-4">
+        <div className="title flex items-center justify-between">
+          <h1 className="text-2xl font-bold">Add Package</h1>
+          <Link to="/dashboard/packages">
+            <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+              Packages Lists
+            </button>
+          </Link>
+        </div>
+
+        <div className="mt-4 p-14 shadow border">
+          <form>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            {/* Input Group GRIDS */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="price"
+                >
+                  Price
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="price"
+                  type="text"
+                  placeholder="Price"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="Origin"
+                >
+                  Origin
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="Origin"
+                  type="text"
+                  placeholder="Origin"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="Destination"
+                >
+                  Destination
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="Destination"
+                  type="text"
+                  placeholder="Destination"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="packageClasses"
+                >
+                  Packages Classes
+                </label>
+                <select className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                  <option value="Luxury">Luxury</option>
+                  <option value="Mid Range">Mid Range</option>
+                  <option value="Budget">Budget</option>
+                </select>
+              </div>
+            </div>
+            {/* input groups GRIDS END */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="description"
+              >
+                Description
+              </label>
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="description"
+                placeholder="Description"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="image"
+              >
+                Image
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="image"
+                type="file"
+                placeholder="Image"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Add Package
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPackage;
