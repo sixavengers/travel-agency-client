3c76979d495e7a0219adf70356d82d9dac635e83
ashikmahmud <ashikmamud187@gmail.com>
2022-11-16 01:44:06 +0600

    updated code from development

---
+++ src/pages/payment/CheckoutForm.tsx
@@ -0,0 +1,48 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button
+        className="btn btn-sm bg-gray-900 text-white px-2 rounded-2xl mt-4"
+        type="submit"
+        disabled={!stripe}
+      >
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -1,26 +1,30 @@
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import React from 'react';
-import CheckoutForm from './CheckoutForm';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
-
-const stripePromise = loadStripe('pk_test_51L0gMADuiIiaFlXNz9N5k6HXIPhnvURpmXcZeRZKvElE1vRaWTc2jUCiZQo6KHnurxrTpsvhinpjyKGlXZRDVFwg00cdBRF6R2');
-
+const stripePromise = loadStripe(
+  "pk_test_51L0gMADuiIiaFlXNz9N5k6HXIPhnvURpmXcZeRZKvElE1vRaWTc2jUCiZQo6KHnurxrTpsvhinpjyKGlXZRDVFwg00cdBRF6R2"
+);
 
 const Payment = () => {
-    return (
-        <div className='bg-[#EBF0F4] pb-12'>
-            <div className='text-center text-lg font-roboto py-5'>
-                <h3>PAY FOR YOUR DESTINATION - <span className='text-[#1E88E5]'>Packege Name</span></h3>
-                <p className=''>Please Pay : <span className='text-red-600'>$120</span></p>
-            </div>
-            <div className='bg-white rounded-2xl shadow-2xl w-96 mx-auto p-8 '>
-                <Elements stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-[#EBF0F4] pb-12">
+      <div className="text-center text-lg font-roboto py-5">
+        <h3>
+          PAY FOR YOUR DESTINATION -{" "}
+          <span className="text-[#1E88E5]">Packege Name</span>
+        </h3>
+        <p className="">
+          Please Pay : <span className="text-red-600">$120</span>
+        </p>
+      </div>
+      <div className="bg-white rounded-2xl shadow-2xl w-96 mx-auto p-8 ">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
