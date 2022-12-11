3c06db20ce692427f3427302a9eaedb00b5e19db
ashikmahmud <ashikmamud187@gmail.com>
2022-12-12 03:54:22 +0600

    setup rtk query with redux-toolkit

---
+++ src/api/AuthenticationApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { server_url } from "../config/config";
+export const authenticationApi = createApi({
+  reducerPath: "AuthenticationApi",
+  refetchOnFocus: true,
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${server_url}/users`,
+  }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (data: any) => ({
+        url: `/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterUserMutation } = authenticationApi;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,5 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { authenticationApi } from "../api/AuthenticationApi";
 import AuthReducer from "../features/auth/AuthSlice";
 
 import counterReducer from "../features/counter/counterSlice";
@@ -7,7 +8,10 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
     authentication: AuthReducer,
+    [authenticationApi.reducerPath]: authenticationApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authenticationApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
